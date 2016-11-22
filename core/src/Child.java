51cf68458c6e27bc10788bddb1a730592c3dcae0
Bryan Hutchinson <bh79@cornell.edu>
2016-11-22 11:04:59 -0500

    wip

---
+++ core/src/Child.java
@@ -0,0 +1,6 @@
+public class Child extends Parent {
+
+    protected void doSomething() {
+        System.out.print("dude, I'm a kid!");
+    }
+}
---
+++ core/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main (String[] args) {
+        Parent object = new Child();
+        object.doSomething();
+    }
+
+}
+
